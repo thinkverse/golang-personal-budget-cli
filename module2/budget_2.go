46fae833de67a5095654acf05ea0fcf294e7c459
Kim Hallberg <hallberg.kim@gmail.com>
2022-10-15 05:27:24 +0200

    Complete Module 3: Working with slices and maps

--- module2/budget_2.go
+++ module2/budget_2.go
@@ -53,6 +53,14 @@ var errDuplicateEntry = errors.New("Cannot add duplicate entry")
 
 // AddItem adds an item to the current budget
 func (b *Budget) AddItem(description string, price float32) error {
+	if (b.CurrentCost() + price) > b.Max {
+		return errDoesNotFitBudget
+	}
+
+	b.Items = append(b.Items, Item{
+		Description: description,
+		Price:       price,
+	})
 
 	return nil
 }
@@ -61,20 +69,36 @@ func (b *Budget) AddItem(description string, price float32) error {
 func (b *Budget) RemoveItem(description string) {
 	for i := range b.Items {
 		if b.Items[i].Description == description {
-
+			b.Items = append(b.Items[:i], b.Items[i+1:]...)
+			break
 		}
 	}
 }
 
 // CreateBudget creates a new budget with a specified max
 func CreateBudget(month time.Month, max float32) (*Budget, error) {
-	var newBudget *Budget
+	if len(report) == 12 {
+		return nil, errReportIsFull
+	}
 
-	return newBudget, nil
+	if _, ok := report[month]; ok {
+		return nil, errDuplicateEntry
+	}
+
+	budget := &Budget{
+		Max: max,
+	}
+
+	report[month] = budget
+
+	return budget, nil
 }
 
 // GetBudget returns budget for given month
 func GetBudget(month time.Month) *Budget {
+	if budget, ok := report[month]; ok {
+		return budget
+	}
 
 	return nil
 }
